81c64101b44888a4a0ab16a002cc7ff718eb929c
heskethg <85505664+heskethg@users.noreply.github.com>
2021-07-23 16:06:29 -0400

    updates

--- src/GUI.py
+++ src/GUI.py
@@ -1,16 +1,17 @@
-import FreeCAD
-import FreeCADGUI
-import Part
-
 import sys
 
-from Pyside.Qtwidgets import QApplication
+from PySide6.QtCore import Qt
+
+from PySide6.QtWidgets import QApplication, QLabel
+
+                                                     
 
 if __name__ == "__main__":
+
     app = QApplication(sys.argv)
-    FreeCADGui.showMainWindow()
-    
-    doc = FreeCAD.newDocument()
-    box = Part.makeBox(100, 100, 100)
-    Part.show(box)
+
+    label = QLabel("Hello World", alignment=Qt.AlignCenter)
+
+    label.show()
+
     sys.exit(app.exec_())
