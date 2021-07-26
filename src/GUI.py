88034b2ff2aba4180d56bbc5c04685d49552f17a
heskethg <85505664+heskethg@users.noreply.github.com>
2021-07-25 21:34:06 -0400

    updates

--- src/GUI.py
+++ src/GUI.py
@@ -1,4 +1,4 @@
-import sys
+""" import sys
 
 from PySide6.QtCore import Qt
 
@@ -12,4 +12,17 @@ if __name__ == "__main__":
 
     label.show()
 
+    sys.exit(app.exec_()) """
+
+import FreeCAD
+import FreeCADGui
+import Part
+import sys
+from PySide6.QtWidgets import QApplication
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    FreeCADGui.showMainWindow()
+    doc = FreeCAD.newDocument()
+    box = Part.makeBox(100, 100, 100)
+    Part.show(box)
     sys.exit(app.exec_())
