2a315462c126c253289ae6d82f6a7dd516224bc1
heskethg <85505664+heskethg@users.noreply.github.com>
2021-07-26 20:47:55 -0400

    successful integration

--- src/GUI.py
+++ src/GUI.py
@@ -3,15 +3,14 @@ import FreeCADGui as Gui
 import Part
 import sys
 
+from PySide2.QtWidgets import QApplication
+
 if __name__ == "__main__":
-    myDoc = App.newDocument("Test1")
-    App.setActiveDocument("Test1")
-    print(App.listDocuments())
-    print(App.activeDocument())
-
-    # Gui.showMainWindow()
-
-    box = Part.makeBox(100, 100, 100)
+    qapp = QApplication(sys.argv)
+    Gui.showMainWindow()
+    
+    doc = App.newDocument()
+    box = Part.makeBox(50, 50, 50)
     Part.show(box)
 
-print(sys.version)
+    sys.exit(qapp.exec_())
